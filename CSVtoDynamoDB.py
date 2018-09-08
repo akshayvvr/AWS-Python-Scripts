e644bbb91c1baacac580d88502fd8392bef9a521
akshayvvr <32500748+akshayvvr@users.noreply.github.com>
2018-09-08 14:02:06 -0700

    Update CSVtoDynamoDB.py

--- CSVtoDynamoDB.py
+++ CSVtoDynamoDB.py
@@ -1,20 +1,25 @@
 import boto3
 import json
 
+#Declare S3 bucket and DynamoDB Boto3 Clients
 s3_client=boto3.client('s3')
 dynamodb=boto3.resource('dynamodb')
 
+#Event is the uploading of a CSV File to a bucket
 def csv_reader(event, context):
-    bucket=event['Records'][0]['s3']['bucket']['name']
     
+    #Fetch the bucket name and the file 
+    bucket=event['Records'][0]['s3']['bucket']['name']
     key=event['Records'][0]['s3']['object']['key']
     print((str(event)))
-    obj=s3_client.get_object(Bucket=bucket, Key=key)
     
+    # Read the Object using get_object API
+    obj=s3_client.get_object(Bucket=bucket, Key=key)
     rows=obj['Body'].read().split('\n')
     
     table=dynamodb.Table('Messages')
     
+    #Write the CSV file to the DynamoDB Table
     with table.batch_writer() as batch:
         for row in rows:
             batch.put_item(Item={
